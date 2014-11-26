f29a3114a172544e892fec1b58540ff83c87ef48
xudChen <xudong.chen@jumacc.com>
2014-11-26 18:49:19 +0800

    update index.html,main.css,mian.js

--- js/main.js
+++ js/main.js
@@ -17,40 +17,24 @@ $(function(){
 	    juma.write("/peripherals/buzzer","5s");
 	}); 
 	
-	
-	// create canvas and context objects
 	var canvas = document.getElementById('picker');
 	var ctx = canvas.getContext('2d');
-	
-	// drawing active image
 	var image = new Image();
-	image.onload = function () {
-	    ctx.drawImage(image, 0, 0, image.width, image.height); // draw the image on the canvas
+	image.onload = function(){
+	    ctx.drawImage(image, 0, 0, image.width, image.height);
 	}
-	
-	// select desired colorwheel
 	var imageSrc = 'img/colorwheel1.png';
 	image.src = imageSrc;
-	
-	$('#picker').click(function(e) { // click event handler
-			e.preventDefault();
-			var canvasOffset = $(canvas).offset();
-	        var canvasX = Math.floor(e.pageX - canvasOffset.left);
-	        var canvasY = Math.floor(e.pageY - canvasOffset.top);
-	
-	        // get current pixel
-	        var imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
-	        var pixel = imageData.data;
-	
-	        // update preview color
-	        var pixelColor = "rgb("+pixel[0]+", "+pixel[1]+", "+pixel[2]+")";
-	        
-      
-	        var dColor = pixel[2] + 256 * pixel[1] + 65536 * pixel[0];
-
-	        $('#hexVal').html(' #' + ('0000' + dColor.toString(16)).substr(-6)+' ');
-	       			
-			juma.write("/peripherals/rgblight",('0000' + dColor.toString(16)).substr(-6));
-
+	$('#picker').click(function(e) {
+		var canvasOffset = $(canvas).offset();
+        var canvasX = Math.floor(e.pageX - canvasOffset.left);
+        var canvasY = Math.floor(e.pageY - canvasOffset.top);
+        var imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
+        var pixel = imageData.data;
+        var pixelColor = "rgb("+pixel[0]+", "+pixel[1]+", "+pixel[2]+")";
+        var dColor = pixel[2] + 256 * pixel[1] + 65536 * pixel[0];
+		$('#hexVal').css('color',' #' + ('0000' + dColor.toString(16)).substr(-6));
+        $('#hexVal').html(' #' + ('0000' + dColor.toString(16)).substr(-6)+' ');
+		juma.write("/peripherals/rgblight",('0000' + dColor.toString(16)).substr(-6));
 	}); 
 });
